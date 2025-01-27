65506402c50892b46ec48082da77bd4dac7c4890
imaneouarzi <ima55ouar@gmail.com>
2025-01-27 17:37:30 +0100

    add login & logout logic

---
+++ dist/js/main copy.js
@@ -0,0 +1,134 @@
+let postContent = document.getElementById("post");
+
+function getPosts(){
+    axios.get('https://tarmeezacademy.com/api/v1/posts')
+  .then(function (response) {
+    let posts = response.data.data
+    for (const post of posts) {
+        let title = (post.title === null || post.title === "null") ? "Title" : post.title;
+
+        let tags = post.tags.map(tag => 
+          `<div class="bg-red-400 rounded-2xl px-5 py-1">${tag}</div>`
+      ).join(''); 
+
+        let element = `<div class="post-infos bg-white rounded-xl py-5 px-16 shadow-xl mb-7">
+                    <div class="user-post flex gap-5">
+                        <img src="images/person-1824144_1280.png" class="w-14">
+                        <div class="username">
+                            <div class="font-bold">${post.author.name}</div>
+                            <div class="text-slate-400 font-bold text-sm py-1">${post.created_at}</div>
+                        </div>
+                    </div>
+                    <img src="images/img3.jpg" class="rounded-xl mt-5 mb-5 h-[600px] w-full " alt="">
+                    <div class="post-title font-bold text-xl">${title}</div>
+                    <div class="post-description mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aspernatur illo quibusdam dolorem blanditiis, odit, iusto accusamus quis quas repudiandae iure
+                        exercitationem consequatur ducimus minus magnam ipsum nemo? Itaque, architecto quaerat.</div>
+                    <i class="fa-solid fa-comments fa-xl" style="color: #000000;"></i> <span><a href=""
+                            class="font-bold px-2"> (${post.comments_count}) Comments</a></span>
+                    <div class="tags flex gap-5 py-4">
+                        ${tags} <!-- Insert the dynamic tags here -->
+                    </div>
+                </div>`;
+
+                 // Convert the string into a DOM element
+                 let tempDiv = document.createElement('div'); // Temporary container
+                 tempDiv.innerHTML = element.trim(); // Convert the string to DOM elements
+
+                 // Append the first child (the actual new element) to the parent
+                 postContent.appendChild(tempDiv.firstChild);
+    }
+    // console.log(posts);
+  })
+  .catch(function (error) {
+    // en cas d’échec de la requête
+    console.log(error);
+  })
+}
+
+getPosts()
+
+function userLogin(){
+
+  let username = document.getElementById("username").value
+  let password = document.getElementById("password").value
+
+
+  axios.post('https://tarmeezacademy.com/api/v1/login', {
+    "username": username,
+    "password": password
+  })
+  .then(function (response) {
+    localStorage.setItem("token",response.data.token)
+    localStorage.setItem("user",JSON.stringify(response.data.user))
+    document.getElementById("modal").classList.add("hidden");
+    document.getElementById("login").innerHTML = `
+                <img src="images/person-1824144_1280.png" class="w-10" alt="">
+                <span class="user-text">${response.data.user.username}</span>
+                <div class="logout" id="logout" >
+                    <button  onclick="logout()"><span class="text-[#FF9696] font-bold">logout</span></button>
+                </div>`;
+
+    if (localStorage.getItem("token")) {
+      document.getElementById("current_username").textContent =response.data.user.username;
+      let user_email = (response.data.user.email === null || response.data.user.email === "null") ? "user@company.com" : response.data.user.email;
+      document.getElementById("current_email").textContent =user_email;
+    }
+   
+    showLoginAlert()
+  //  hideAlert()
+    console.log(response.data.token);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+
+function showLoginAlert() {
+  const alert = document.getElementById('alert');
+  alert.classList.remove('opacity-0', 'pointer-events-none');
+  alert.classList.add('opacity-100', 'pointer-events-auto');
+}
+function hideAlert() {
+  const closeAlertBtn = document.getElementById('closeAlert');
+  // Fermer l'alerte
+  closeAlertBtn.addEventListener('click', () => {
+    alert.classList.remove('opacity-100', 'pointer-events-auto');
+    alert.classList.add('opacity-0', 'pointer-events-none');
+});
+}
+
+function setupUI() {
+  if (localStorage.getItem("token") != null) {
+    const userData = localStorage.getItem("user")
+  
+    // Parse the JSON string into an object
+    const userObject = JSON.parse(userData);
+    
+  
+    document.getElementById("login").innerHTML = `
+    <img src="images/person-1824144_1280.png" class="w-10" alt="">
+    <span class="user-text">${userObject.username}</span>
+    <div class="logout" id="logout">
+        <button  onclick="logout()"><span class="text-[#FF9696] font-bold">logout</span></button>
+    </div>`;
+  
+    document.getElementById("current_username").textContent =userObject.username;
+    let user_email = (userObject.email === null || userObject.email === "null") ? "user@company.com" : userObject.email;
+    document.getElementById("current_email").textContent =user_email;
+  }
+}
+setupUI()
+
+function logout() {
+  localStorage.removeItem("token")
+  localStorage.removeItem("user")
+  document.getElementById("login").innerHTML = `
+  <button id="open-modal-btn"><span
+                        class="text-[#FF9696] font-bold border-2 border-[#FF9696] px-5 py-1 rounded-lg hover:bg-[#FF9696] hover:text-zinc-50">login</span></button>
+                   <button id="open-modal-btn-register"><span
+                        class="text-[#FF9696] font-bold border-2 border-[#FF9696] px-5 py-1 rounded-lg hover:bg-[#FF9696] hover:text-zinc-50">Register</span></button>
+                `;
+  setupUI()
+  console.log("logout clicked")
+}
---
+++ dist/js/modal.js
@@ -0,0 +1,18 @@
+// Login Modal
+document.getElementById("open-modal-btn").addEventListener("click", function() {
+    document.getElementById("modal").classList.remove("hidden");
+     console.log("clicked")
+  });
+  
+  document.getElementById("close-modal-btn").addEventListener("click", function() {
+    document.getElementById("modal").classList.add("hidden");
+  });
+// Register Modal
+  document.getElementById("open-modal-btn-register").addEventListener("click", function() {
+    document.getElementById("modal-register").classList.remove("hidden");
+    // console.log("clicked")
+  });
+  
+  document.getElementById("close-modal-btn-register").addEventListener("click", function() {
+    document.getElementById("modal-register").classList.add("hidden");
+  });
